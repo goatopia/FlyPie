e0f50d12cbdff65e542fbc5e21b5b50cb19ac1b3
Frayhem <69558183+Frayhem@users.noreply.github.com>
2022-12-11 19:54:52 -0500

    updated the fly spawner
    just added a destroy function as soon as the fly collides to the pie.

--- ProjectFlyPie/Assets/FlyCollider.cs
+++ ProjectFlyPie/Assets/FlyCollider.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class FlyCollider : MonoBehaviour
 {
+    public GameObject currentFly;
+
     public FlyNPieSpawner spawnerscript;
     private void OnCollisionEnter(Collision collision)
     {
         GameObject other = collision.gameObject;
         if (other.CompareTag("Pie"))
         {
+            currentFly = GameObject.FindGameObjectWithTag("Fly");
             spawnerscript = GameObject.FindObjectOfType<FlyNPieSpawner>();
             spawnerscript.spawnFly();
+            Destroy(currentFly);
             Debug.Log("the fly reached the pie");
         }
     }
